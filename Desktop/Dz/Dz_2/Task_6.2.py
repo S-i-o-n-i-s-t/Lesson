56e3005a07d5688aed697f5cdcf4ac22e0bb41f5
S-i-o-n-i-s-t <metalljan@yandex.ru>
2022-09-27 21:57:35 +0300

    Dz_2

---
+++ Desktop/Dz/Dz_2/Task_6.2.py
@@ -0,0 +1,31 @@
+# 1. Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр.
+# in -> out
+# - 6782 -> 23
+# - 0.67 -> 13
+# - 198.45 -> 27
+print('Введите вещественное число')
+num = float(input())
+a = 0
+if num < 0:
+    num = num * -1
+while num % 10 != 0:
+    num = num * 10
+num = int(num)
+num = num // 10
+print(num)
+N = str(num)
+size = len(N)
+while size != 0:
+    a = a + num % 10
+    num = num // 10
+    size = size - 1
+print(f'Сумма цифр в числе = {a}')
+
+
+
+
+ 
+
+
+
+
